abffa7f67a2dcac8c62d603a8c9b17e6e8df6e07
codeaches <info001@codeaches.com>
2019-12-28 15:52:56 -0700

    Spring Boot Actuator Custom DB Health Check Example [http://codeaches.com/]

--- src/main/java/com/codeaches/actuator/DBConfig.java
+++ src/main/java/com/codeaches/actuator/DBConfig.java
@@ -2,11 +2,7 @@ package com.codeaches.actuator;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.actuate.health.HealthIndicator;
-import org.springframework.boot.actuate.jdbc.DataSourceHealthIndicator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -26,25 +22,14 @@ public class DBConfig {
   @Value("${spring.smartdb.datasource.password}")
   String password;
 
-  @Bean(name = "smartDB")
-  public DataSource bdrDataSource() {
+  @Bean(name = "smartdb")
+  public DataSource smartdb() {
 
-    DriverManagerDataSource dataSource = new DriverManagerDataSource();
-    dataSource.setDriverClassName(driverClassName);
-    dataSource.setUrl(url);
-    dataSource.setUsername(username);
-    dataSource.setPassword(password);
-    return dataSource;
-  }
-
-  @Value("${spring.smartdb.datasource.validationquery}")
-  String validationquery;
-
-  @Bean("smartDBHealthIndicator")
-  public HealthIndicator smartDBHealthIndicator(@Autowired @Qualifier("smartDB") DataSource dataSource) {
-
-    DataSourceHealthIndicator indicator = new DataSourceHealthIndicator(dataSource);
-    indicator.setQuery(validationquery);
-    return indicator;
+    DriverManagerDataSource ds = new DriverManagerDataSource();
+    ds.setDriverClassName(driverClassName);
+    ds.setUsername(username);
+    ds.setPassword(password);
+    ds.setUrl(url);
+    return ds;
   }
 }
